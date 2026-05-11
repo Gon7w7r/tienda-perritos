c0ecee87af36f12753d9a44b2f79182aabc38474
GonzaloNavarrete <gon.navarrete@duocuc.cl>
2026-05-11 15:51:05 -0400

    Reinicializar db para prueba de pipeline v4

--- db/init.sql
+++ db/init.sql
@@ -8,7 +8,7 @@ CREATE TABLE IF NOT EXISTS productos (
     precio DECIMAL(10,2) NOT NULL,
     stock INT NOT NULL
 );
---PRUEBA DE PIPELINE reinicio
+-- PRUEBA DE PIPELINE reinicio2
 
 INSERT INTO productos (nombre, descripcion, precio, stock) VALUES
 ('Alimento Cachorro Premium Pipeline test', 'Sabor a pollo, razas pequenas', 19990, 15),
